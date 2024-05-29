b002cf1cd8f2e360b94e7c943a3234ad3ac8620d
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2024-05-29 17:25:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,98 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+let i = 0;
+//let txt = 'Extreme Performance'; /* The text */
+let speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter(txt, i = 0, speed = 100) {  // Default values for i and speed
+  if (i < txt.length) {
+    document.getElementById("type").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(() => typeWriter(txt, i, speed), speed);  // Pass all variables needed
+  }
+}
+
+function clear(){
+  document.getElementById("type").innerHTML = "";
+}
+
+
+
+function showRange() {
+  clear()
+  typeWriter("Humanity's")
+
+
+}
+
+function showDesign() {
+  clear()
+  typeWriter('Future')
+
+
+}
+
+function showFuture() {
+  clear()
+  typeWriter('De Futuro Ipsum')
+
+
+}
+
+
+typeWriter('Engineering'); 
+
+setTimeout(showRange, 2000);
+
+setTimeout(showDesign, 4000);
+
+setTimeout(showFuture, 6000);
+
+
+
+let opacity = 0; 
+let intervalID = 0; 
+
+
+function fadeIn() { 
+    setInterval(show, 200); 
+} 
+
+function show() { 
+    var body = document.getElementById("fade"); 
+    opacity = Number(window.getComputedStyle(body) 
+                    .getPropertyValue("opacity")); 
+    if (opacity < 1) { 
+        opacity = opacity + 0.1; 
+        body.style.opacity = opacity 
+    } else { 
+        clearInterval(intervalID); 
+    } 
+} 
+
+setTimeout(fadeIn, 8000);
+
+
+
+
+document.addEventListener("visibilitychange", () => {
+   if (document.hidden) {
+     for (var i = 1; i <10; i++)
+        window.clearInterval(i);
+      clear()
+   } else {
+     clear()
+     document.getElementById("type").innerHTML = "De Futuro Ipsum";
+     show()
+      // tab is active
+   }
+});
